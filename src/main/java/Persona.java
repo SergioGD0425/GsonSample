f36b0c8e67d52831f724b7c41ebc695aa9ebff7c
Sergio Garcia <sergiogarciadelgado25@gmail.com>
2019-09-20 10:08:36 +0100

    first commit

---
+++ src/main/java/Persona.java
@@ -0,0 +1,7 @@
+
+public class Persona {
+	String nombre;
+	String apellidos;
+	Integer edad;
+}
+
---
+++ src/main/java/main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class main {
+
+	public static void main(String[] args) {
+		Persona p = new Persona();
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce un nombre:");
+		p.nombre = entrada.nextLine();
+		System.out.println("Introduce apellidos:");
+		p.apellidos = entrada.nextLine();
+		System.out.println("Introduce edad:");
+		p.edad = entrada.nextInt();
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(p);
+		System.out.println(json);
+		
+		entrada.close();
+	}
+
+}
